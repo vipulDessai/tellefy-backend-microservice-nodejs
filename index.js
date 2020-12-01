090d3bfde472531b11896f79c9749f70a5527127
Vipul Dessai <badgamerbad@gmail.com>
2020-12-01 17:46:33 +0530

    added cors support middleware for all routes

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-const express = require("express");
-const bodyparser = require("body-parser");
-const path = require("path");
-const http = require("http");
+// read the .env file
+require('dotenv').config();
+
+const express = require('express');
+const bodyparser = require('body-parser');
+const path = require('path');
+const http = require('http');
 const app = express();
 
-const frontEndEntryFileLocation = path.join(__dirname, "../", "tellefy-frontend-angular", "dist", "tellefy-frontend-angular", "index.html");
-const frontEndEDistFolderLocation = path.join(__dirname, "../", "tellefy-frontend-angular", "dist", "tellefy-frontend-angular");
-
 // api file for interacting with MongoDB
-const account = require("./server/routes/account");
+const account = require('./server/routes/account');
 
 // parsers
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 
-// angular DIST output folder
-app.use(express.static(frontEndEDistFolderLocation));
+// set headers
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', `${process.env.FRONTEND_SSL_ENABLED == "true" ? 'https' : 'http'}://${process.env.FRONTEND_URL}`);
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 // account location
-app.use("/account", account);
-
-app.get('*', (req, res) => {
-    res.sendFile(frontEndEntryFileLocation);
-});
+app.use('/account', account);
 
 // set port
 const port = process.env.PORT || 3000;
-app.set("port", port);
+app.set('port', port);
 
 const server = http.createServer(app);
 server.listen(port, () => console.log(`Running on localhost:${port}`));
