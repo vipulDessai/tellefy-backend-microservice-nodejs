ba14c2d2096a2bdc1649ae58e29e79a65a278cb3
Vipul Dessai <badgamerbad@gmail.com>
2020-12-06 16:57:08 +0530

    added dummy register route

--- server/routes/account.js
+++ server/routes/account.js
@@ -4,7 +4,6 @@ const router = express.Router();
 router.get("/", (req, res) => {
     res.send({"foo": "dummy users get"});
 });
-
 router.post("/authenticate", (req, res) => {
     res.send({
         userName: "foo_bar",
@@ -13,7 +12,14 @@ router.post("/authenticate", (req, res) => {
         lastLogin: new Date(),
     });
 });
-
+router.post("/register", (req, res) => {
+    res.send({
+        userName: "foo_bar",
+        firstName: "foo",
+        lastName: "bar",
+        lastLogin: new Date(),
+    });
+});
 router.delete("/:index", (req, res) => {
     res.send({"foo": "dummy users delete"});
 });
